6363d7247269ecc6bed217bb11fa3a0da385128b
Amaanuddin05 <amaanuddin0501@gmail.com>
2025-06-24 22:55:15 +0530

    basic AnalyzeComponent done with static data

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UploadComponent } from './upload/upload.component';
+import { AnalyzeComponent } from './analyze/analyze.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   {path:'upload', component: UploadComponent},
+  {path:'analyze', component: AnalyzeComponent},
   { path: '**', redirectTo: '' }
 ];
