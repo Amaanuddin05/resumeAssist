af671ba2f430ab3a18ca88caebe0e2e4bb0a48f0
Amaanuddin05 <amaanuddin0501@gmail.com>
2025-06-25 14:35:48 +0530

    basic ReportComponent done

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UploadComponent } from './upload/upload.component';
 import { AnalyzeComponent } from './analyze/analyze.component';
+import { ReportComponent } from './report/report.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   {path:'upload', component: UploadComponent},
   {path:'analyze', component: AnalyzeComponent},
+  {path:'report', component: ReportComponent},
   { path: '**', redirectTo: '' }
 ];
